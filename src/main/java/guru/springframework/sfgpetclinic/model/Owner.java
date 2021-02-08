3a825b60d62e941fc0ec6e4c906a6d791b25e06b
Mario J. Kofler <mario.j.kofler@runbox.com>
2021-02-08 20:43:01 +0100

    adding POJOs

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by mjk on 08.02.21
+ */
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by mjk on 08.02.21
+ */
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
